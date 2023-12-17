a4173cfefb972ee9126ef68fdfdb9eb39f5934f3
Aminsfnhdez <digimonataque@gmail.com>
2023-12-17 16:29:46 -0500

    Vista General Conexión BD

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useEffect, useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import axios from "axios";
 function App() {
+  const baseUrl = "http://localhost:80/apiFrameworks/";
+  const [data, setData] = useState([]);
+
+  const peticionGet=async()=>{
+    await axios.get(baseUrl)
+    .then(response=>{
+      setData(response.data);
+    })
+  }
+
+  useEffect(()=>{
+    peticionGet();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Lanzamiento</th>
+            <th>Desarrollador</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(framework=>(
+            <tr key={framework.id}>
+              <td>{framework.id}</td>
+              <td>{framework.nombre}</td>
+              <td>{framework.lanzamiento}</td>
+              <td>{framework.desarrollador}</td>
+              <td>
+                <button>Editar</button>
+                <button>Eliminar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+     
     </div>
   );
 }
